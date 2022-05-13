55b823003a463aef29b31903ae30281bea216e46
IIGGSS <tvorchh@mail.ru>
2022-05-14 04:46:49 +0900

    hw5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from './styles.module.css';
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    return (
+        <div className={s.header}>
+            {/*// add NavLinks*/}
+            <div className={s.object}></div>
+            <div className={s.linksContainer}>
+                <NavLink className={s.link} to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
+                <NavLink className={s.link} to={PATH.JUNIOR}>Junior</NavLink>
+                <NavLink className={s.link} to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink>
+            </div>
         </div>
     )
 }
